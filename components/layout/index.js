91556cc8745551341817837c517a48076645304d
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-18 09:35:04 +0300

    render the main and footer components

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import Navbar from "./navbar";
+import Footer from "./footer";
 
-export default function Layout() {
-  return <Navbar />;
+export default function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
 }
+
+//#FB0303
