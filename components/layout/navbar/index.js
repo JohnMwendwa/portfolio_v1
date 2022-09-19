68b79a256e240af4801082426ca07e8511d899cb
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-19 23:17:00 +0300

    style the navbar and make it responsive

---
+++ components/layout/navbar/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import styled from "styled-components";
+
+const NavWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: yellow;
+  border-bottom: 1px solid black;
+  font-size: 1.25rem;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
+`;
+const Logo = styled.div`
+  padding-left: 10px;
+  min-width: 130px;
+`;
+const LogoImage = styled(Image)`
+  width: 100%;
+  min-width: 130px;
+`;
+const NavItems = styled.ul`
+  display: flex;
+  list-style: none;
+  margin: 0 20px;
+  padding: 0px;
+`;
+const NavItem = styled.li`
+  padding: 20px 0;
+  padding-right: 20px;
+  &:first-child {
+    padding-left: 20px;
+  }
+`;
+
+export default function Navbar() {
+  return (
+    <NavWrapper>
+      <Logo>
+        <LogoImage src="/logo.svg" alt="logo" width={256} height={79} />
+      </Logo>
+      <NavItems>
+        <NavItem>
+          <Link href="/projects">
+            <a>Projects</a>
+          </Link>
+        </NavItem>
+
+        <NavItem>
+          <Link href="https://blog.johnmwendwa.me">
+            <a>Blog</a>
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </NavItem>
+      </NavItems>
+    </NavWrapper>
+  );
+}
