f964fc5c1666c7f1896521f6b9b7572f00f165a9
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-21 00:00:05 +0300

    wrap the whole app in a flex container

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,14 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
 import Navbar from "./navbar";
 import Footer from "./footer";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
 export default function Layout({ children }) {
   return (
-    <>
+    <Container>
       <Navbar />
       <main>{children}</main>
       <Footer />
-    </>
+    </Container>
   );
 }
 
