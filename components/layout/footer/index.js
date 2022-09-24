3e7f3528a731521d795ff85754a4ad369a1a31c7
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-25 00:54:45 +0300

    style the footer component

--- components/layout/footer/index.js
+++ components/layout/footer/index.js
@@ -2,8 +2,30 @@ import React from "react";
 import styled from "styled-components";
 
 const FooterWrapper = styled.footer`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   margin-top: auto;
+  background-color: #101112;
+  color: white;
 `;
+const CopyrightWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border-top: 1px solid rgba(255, 255, 255, 0.5);
+  padding: 20px 30px;
+  color: rgba(255, 255, 255, 0.5);
+`;
+
+const Copyright = styled.div``;
+
 export default function Footer() {
-  return <FooterWrapper>Footer</FooterWrapper>;
+  return (
+    <FooterWrapper>
+      <CopyrightWrapper>
+        <Copyright>&copy; {new Date().getFullYear()}</Copyright>
+        <div>John Mwendwa</div>
+      </CopyrightWrapper>
+    </FooterWrapper>
+  );
 }
