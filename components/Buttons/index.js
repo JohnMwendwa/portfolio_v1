6dfb063c6dc23f88b610a30e480bc61ab40c6603
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-28 22:48:03 +0300

    remove the empty transform style to get rid of client and server side stylesheet mismatch

--- components/Buttons/index.js
+++ components/Buttons/index.js
@@ -44,7 +44,6 @@ export const BackToTopBtn = () => {
           viewBox="0 0 24 24"
           style={{
             fill: "rgba(0, 0, 0, 1)",
-            transform: "",
           }}
         >
           <path d="M6 4H18V6H6zM11 14L11 20 13 20 13 14 18 14 12 8 6 14z"></path>
