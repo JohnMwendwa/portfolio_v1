10e8aeb0cc0f57c3dbc48b10b00743ca3098470f
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-28 22:16:17 +0300

    render the bac-to-top button

--- components/layout/footer/index.js
+++ components/layout/footer/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import { BackToTopBtn } from "../../Buttons";
+
 const FooterWrapper = styled.footer`
   display: flex;
   flex-direction: column;
@@ -12,9 +14,11 @@ const FooterWrapper = styled.footer`
 const CopyrightWrapper = styled.div`
   display: flex;
   justify-content: space-between;
+  position: relative;
   border-top: 1px solid rgba(255, 255, 255, 0.5);
   padding: 20px 30px;
   color: rgba(255, 255, 255, 0.5);
+  line-height: 2.8rem;
 `;
 
 const Copyright = styled.div``;
@@ -23,6 +27,7 @@ export default function Footer() {
   return (
     <FooterWrapper>
       <CopyrightWrapper>
+        <BackToTopBtn />
         <Copyright>&copy; {new Date().getFullYear()}</Copyright>
         <div>John Mwendwa</div>
       </CopyrightWrapper>
