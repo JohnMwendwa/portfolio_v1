657d821c0f4446b5abe85586a549cb7bf3d5f054
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-02 12:26:14 +0300

    export default colors

---
+++ components/theme/colors.js
@@ -0,0 +1,8 @@
+export default colors = {
+  bgColor: "#F2CE16",
+  primaryColor: "#F2BC1B",
+  secondaryColor: "#D97A07",
+  tertiaryColor: "#593C19",
+  textColor: "#ffffff",
+  secondaryTextColor: "#BFBFBF",
+};
