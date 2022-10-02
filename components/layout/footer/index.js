e42b1e98ab238155bf8ecbf8ef3e05b9e9c9919c
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-02 09:00:55 +0300

    import and render the address component

--- components/layout/footer/index.js
+++ components/layout/footer/index.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { useRouter } from "next/router";
 
 import { BackToTopBtn } from "../../Buttons";
+import Address from "../../address/Address";
 
 const FooterWrapper = styled.footer`
   display: flex;
@@ -23,62 +24,13 @@ const CopyrightWrapper = styled.div`
 `;
 
 const Copyright = styled.div``;
-const Address = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  padding: 45px 0;
-
-  & h5 {
-    margin: 10px 0;
-    font-size: 1.125em;
-  }
-  & p,
-  a {
-    margin: 0;
-    font-size: 0.825em;
-    margin-bottom: 20px;
-  }
-
-  @media (min-width: 768px) {
-    flex-direction: row;
-    justify-content: space-evenly;
-
-    & p {
-      margin: 0;
-    }
-  }
-`;
 
 export default function Footer() {
   const router = useRouter();
   const path = router.route;
   return (
     <FooterWrapper>
-      {path !== "/contact" ? (
-        <Address>
-          <div>
-            <h5>Address</h5>
-            <p>Nairobi, kenya</p>
-          </div>
-          <div>
-            <h5>Email</h5>
-            <p>
-              <a href="mailto:dev.johnmwendwa@gmail.com">
-                dev@johnmwendwa@gmail.com
-              </a>
-            </p>
-          </div>
-          <div>
-            <h5>Phone</h5>
-            <a href="tel:+254716237927">+254716237927</a>
-          </div>
-        </Address>
-      ) : (
-        ""
-      )}
+      {path !== "/contact" ? <Address /> : ""}
       <CopyrightWrapper>
         <BackToTopBtn />
         <Copyright>&copy; John Mwendwa {new Date().getFullYear()}</Copyright>
