fa5651c5e99401218b9bd431c66f578ad338222c
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-02 08:59:04 +0300

    export the default address component

---
+++ components/address/Address.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+const AddressContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 45px 0;
+
+  & h5 {
+    margin: 10px 0;
+    font-size: 1.125em;
+  }
+  & p,
+  a {
+    margin: 0;
+    font-size: 0.825em;
+    margin-bottom: 20px;
+  }
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-evenly;
+
+    & p {
+      margin: 0;
+    }
+  }
+`;
+
+export default function Address() {
+  return (
+    <AddressContainer>
+      <div>
+        <h5>Address</h5>
+        <p>Nairobi, kenya</p>
+      </div>
+      <div>
+        <h5>Email</h5>
+        <p>
+          <a href="mailto:dev.johnmwendwa@gmail.com">
+            dev@johnmwendwa@gmail.com
+          </a>
+        </p>
+      </div>
+      <div>
+        <h5>Phone</h5>
+        <a href="tel:+254716237927">+254716237927</a>
+      </div>
+    </AddressContainer>
+  );
+}
