257d22fbc72d6e699f9ca763d0d1e828389a5f8f
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-02 11:56:03 +0300

    style the about component

--- components/About.js
+++ components/About.js
@@ -1,5 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+
+const AboutContainer = styled.div`
+  padding: 100px 0;
+  text-align: center;
+  & p {
+    font-size: 0.9375em;
+    font-weight: 400;
+    text-transform: uppercase;
+    color: #aaaaaa;
+    letter-spacing: 3px;
+    margin-bottom: 0;
+  }
+
+  & h2 {
+    font-size: 2.625em;
+    margin: 20px 0 10px;
+  }
+`;
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <AboutContainer>
+      <p>Who I am</p>
+      <h2>About Me</h2>
+    </AboutContainer>
+  );
 }
