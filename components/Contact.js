f3856a38d69811ce67d22573d1ecc085555d424b
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-02 09:26:37 +0300

    style the contact page

--- components/Contact.js
+++ components/Contact.js
@@ -3,6 +3,23 @@ import Address from "./address/Address";
 
 const Wrapper = styled.div`
   text-align: center;
+  padding: 100px 0;
+  background-color: yellow;
+  > p {
+    text-transform: uppercase;
+    font-size: 0.9375em;
+    font-weight: 400;
+    color: #aaaaaa;
+    margin: 0 0 10px;
+    letter-spacing: 2px;
+  }
+
+  > h2 {
+    font-size: 2.625em;
+    margin-bottom: 0px;
+    font-weight: 700;
+    line-height: 1;
+  }
 `;
 
 export default function Contact() {
