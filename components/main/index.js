7550912dfb3e64e7ec80320c0ff292dc9b07b82b
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-12 11:31:42 +0300

    import and initialize aos library

--- components/main/index.js
+++ components/main/index.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import Profile from "./Profile";
 import Services from "./Services";
 import Projects from "./Projects";
 
 export default function Main() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
+
   return (
     <>
       <Profile />
