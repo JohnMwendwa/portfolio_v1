e64410378ded8936856a43e90e98037d5e4388ce
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-02-19 19:59:59 +0300

    remove unnecessary div element

--- components/layout/index.js
+++ components/layout/index.js
@@ -4,19 +4,13 @@ import styled from "styled-components";
 import Navbar from "./navbar";
 import Footer from "./footer";
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-`;
-
 export default function Layout({ children }) {
   return (
-    <Container>
+    <>
       <Navbar />
       <main>{children}</main>
       <Footer />
-    </Container>
+    </>
   );
 }
 
