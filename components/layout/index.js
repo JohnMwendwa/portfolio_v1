c29ef79eb212155a1c6239fe06362c60f2b8d8d9
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-04-07 15:02:11 +0300

    add a ma-width for the main content section

--- components/layout/index.js
+++ components/layout/index.js
@@ -4,11 +4,16 @@ import styled from "styled-components";
 import Navbar from "./navbar";
 import Footer from "./footer";
 
+const Main = styled.main`
+  max-width: 80em;
+  margin: 0 auto;
+`;
+
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <main>{children}</main>
+      <Main>{children}</Main>
       <Footer />
     </>
   );
