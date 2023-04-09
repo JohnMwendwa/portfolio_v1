2ff11811c27064d2b3d17c8b835f94929296f1a4
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-04-09 22:47:19 +0300

    create a landing container

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 import Navbar from "./navbar";
 import Footer from "./footer";
+import Profile from "../main/Profile";
 
 const Main = styled.main`
   max-width: 80em;
   margin: 0 auto;
 `;
-
+const LandingContainer = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr;
+  min-height: 100vh;
+  background: -webkit-linear-gradient(#14142b, #00000069);
+`;
 export default function Layout({ children }) {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
   return (
     <>
-      <Navbar />
+      <LandingContainer>
+        <Navbar />
+        <Profile />
+      </LandingContainer>
       <Main>{children}</Main>
       <Footer />
     </>
