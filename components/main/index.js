dd21bdc7a621f0673d1914a13b8637a270f6b04a
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-04-09 22:46:40 +0300

    move the profile component to the landing container

--- components/main/index.js
+++ components/main/index.js
@@ -1,21 +1,11 @@
-import React, { useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import React from "react";
 
-import Profile from "./Profile";
 import Services from "./Services";
 import Projects from "./Projects";
 
 export default function Main() {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000,
-    });
-  }, []);
-
   return (
     <>
-      <Profile />
       <Services />
       <Projects />
     </>
